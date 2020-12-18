d86ea917bd6e0e461fde47185832a8eb3d6a66a8
Jaan Lambert <jaan.lambert@gmail.com>
2020-12-18 20:36:28 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+import fetch from 'node-fetch'
+import express from 'express'
+
+const app = express()
+const PORT = 3000
+
+app.use(express.static('./static'))
+
+app.get('/search', async (req, res) => {
+  const { term } = req.query
+  const result = await fetch(`https://itunes.apple.com/search?term=${term}`)
+
+  const json = await result.json()
+  console.log(json)
+
+  res.json({ status: 'ok', json }).end()
+})
+
+app.listen(PORT)
+
+console.log(`Running on http://localhost:${PORT}`)
