2fdee55ca12312c76b960324d943fdf048a9df1a
Jaan Lambert <jaan.lambert@gmail.com>
2020-12-19 16:44:58 +0200

    Fix App type

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 
-export const App: React.FC<void> = () => {
+export const App: React.FC = () => {
   const [search, setSearch] = React.useState('')
   const [result, setResult] = React.useState('')
 
