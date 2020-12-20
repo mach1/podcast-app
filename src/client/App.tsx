cfb84088ffe968c20af1cfbcda5a4d39574c01a1
Jaan Lambert <jaan.lambert@gmail.com>
2020-12-20 17:53:26 +0200

    Extract TopBar to separate component

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,10 +1,9 @@
 import { debounce } from 'lodash'
 import * as React from 'react'
-import { AppBar, Toolbar, InputBase, Typography, Container } from '@material-ui/core'
-import { Search as SearchIcon } from '@material-ui/icons'
-import styled from '@emotion/styled'
-import { createMuiTheme, ThemeProvider as MuiThemeProvider, fade } from '@material-ui/core/styles'
+import { Container } from '@material-ui/core'
+import { createMuiTheme, ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles'
 import { ThemeProvider } from '@emotion/react'
+import TopBar from './TopBar'
 
 const theme = createMuiTheme()
 
@@ -21,86 +20,9 @@ export const App: React.FC = () => {
     <MuiThemeProvider theme={theme}>
       <ThemeProvider theme={theme}>
         <Container>
-          <AppBarContainer>
-            <AppBar position='static'>
-              <EnhancedToolbar>
-                <Title>Podcasts</Title>
-                <SearchContainer>
-                  <SearchIconContainer>
-                    <SearchIcon />
-                  </SearchIconContainer>
-                  <StyledInput placeholder='Search...' onChange={onSearchChange} />
-                </SearchContainer>
-              </EnhancedToolbar>
-            </AppBar>
-          </AppBarContainer>
+          <TopBar onSearchChange={onSearchChange} />
         </Container>
       </ThemeProvider>
     </MuiThemeProvider>
   )
 }
-
-const AppBarContainer = styled.div`
-  flex-grow: 1;
-`
-
-const EnhancedToolbar = styled(Toolbar)`
-  justify-content: space-between;
-`
-
-const SearchContainer = styled.div`
-  ${({ theme }) => `
-    position: relative;
-    border-radius: ${theme.shape.borderRadius}px; 
-    background-color: ${fade(theme.palette.common.white, 0.15)};
-    &:hover {
-      background-color: ${fade(theme.palette.common.white, 0.25)};
-    }
-    margin-right: ${theme.spacing(2)}px;
-    margin-left: 0;
-    width: 100%;
-    ${theme.breakpoints.up('sm')} {
-      margin-left: ${theme.spacing(3)}px;
-      width: auto;
-    }
-  `}
-`
-
-const StyledInput = styled(InputBase)`
-  ${({ theme }) => `
-    input {
-      color: inherit;
-      padding: ${theme.spacing(1, 1, 1, 0)};
-      padding-left: calc(1em + ${theme.spacing(4)}px);
-      transition: ${theme.transitions.create('width')};
-      width: 100%;
-      ${theme.breakpoints.up('md')} {
-        width: 12ch;
-        &:focus {
-          width: 20ch;
-        }
-      }
-    }
-  `}
-`
-
-const SearchIconContainer = styled(SearchIcon)`
-  ${({ theme }) => `
-    padding: ${theme.spacing(0, 2)};
-    height: 100%;
-    position: absolute;
-    pointer-events: none;
-    display: flex;
-    alignItems: center;
-    justifyContent: center;
-  `}
-`
-
-const Title = styled(Typography)`
-  ${({ theme }) => `
-    display: none;
-    ${theme.breakpoints.up('sm')} {
-      display: block;
-    },
-  `}
-`
---
+++ src/client/TopBar.tsx
@@ -0,0 +1,95 @@
+import * as React from 'react'
+
+import { AppBar, Toolbar, InputBase, Typography } from '@material-ui/core'
+import { fade } from '@material-ui/core/styles'
+import { Search as SearchIcon } from '@material-ui/icons'
+import styled from '@emotion/styled'
+
+interface Props {
+  onSearchChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+const TopBar = ({ onSearchChange }: Props): React.ReactElement => {
+  return (
+    <AppBarContainer>
+      <AppBar position='static'>
+        <EnhancedToolbar>
+          <Title>Podcasts</Title>
+          <SearchContainer>
+            <SearchIconContainer>
+              <SearchIcon />
+            </SearchIconContainer>
+            <StyledInput placeholder='Search...' onChange={onSearchChange} />
+          </SearchContainer>
+        </EnhancedToolbar>
+      </AppBar>
+    </AppBarContainer>
+  )
+}
+
+const AppBarContainer = styled.div`
+  flex-grow: 1;
+`
+
+const EnhancedToolbar = styled(Toolbar)`
+  justify-content: space-between;
+`
+
+const SearchContainer = styled.div`
+  ${({ theme }) => `
+    position: relative;
+    border-radius: ${theme.shape.borderRadius}px; 
+    background-color: ${fade(theme.palette.common.white, 0.15)};
+    &:hover {
+      background-color: ${fade(theme.palette.common.white, 0.25)};
+    }
+    margin-right: ${theme.spacing(2)}px;
+    margin-left: 0;
+    width: 100%;
+    ${theme.breakpoints.up('sm')} {
+      margin-left: ${theme.spacing(3)}px;
+      width: auto;
+    }
+  `}
+`
+
+const StyledInput = styled(InputBase)`
+  ${({ theme }) => `
+    input {
+      color: inherit;
+      padding: ${theme.spacing(1, 1, 1, 0)};
+      padding-left: calc(1em + ${theme.spacing(4)}px);
+      transition: ${theme.transitions.create('width')};
+      width: 100%;
+      ${theme.breakpoints.up('md')} {
+        width: 12ch;
+        &:focus {
+          width: 20ch;
+        }
+      }
+    }
+  `}
+`
+
+const SearchIconContainer = styled(SearchIcon)`
+  ${({ theme }) => `
+    padding: ${theme.spacing(0, 2)};
+    height: 100%;
+    position: absolute;
+    pointer-events: none;
+    display: flex;
+    alignItems: center;
+    justifyContent: center;
+  `}
+`
+
+const Title = styled(Typography)`
+  ${({ theme }) => `
+    display: none;
+    ${theme.breakpoints.up('sm')} {
+      display: block;
+    },
+  `}
+`
+
+export default TopBar
