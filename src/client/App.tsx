8833bb7d2e38f46fb172bfd8c8a197dc2c44f6a7
Jaan Lambert <jaan.lambert@gmail.com>
2020-12-20 17:42:38 +0200

    Fix AppBar styles

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,53 +1,106 @@
+import { debounce } from 'lodash'
 import * as React from 'react'
-import { AppBar, Toolbar, InputBase, TextField, Button, Container } from '@material-ui/core'
+import { AppBar, Toolbar, InputBase, Typography, Container } from '@material-ui/core'
 import { Search as SearchIcon } from '@material-ui/icons'
 import styled from '@emotion/styled'
-import { createMuiTheme, ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles'
+import { createMuiTheme, ThemeProvider as MuiThemeProvider, fade } from '@material-ui/core/styles'
 import { ThemeProvider } from '@emotion/react'
 
 const theme = createMuiTheme()
 
 export const App: React.FC = () => {
-  const [search, setSearch] = React.useState('')
   const [result, setResult] = React.useState('')
 
-  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearch(event.target.value)
-  }
-
-  const onClickSearch = async () => {
-    const response = await fetch('/search?term=' + encodeURIComponent(search))
+  const onSearchChange = debounce(async (event: React.ChangeEvent<HTMLInputElement>) => {
+    const response = await fetch('/search?term=' + encodeURIComponent(event.target.value))
     const json = await response.json()
     setResult(JSON.stringify(json))
-  }
+  }, 200)
 
   return (
     <MuiThemeProvider theme={theme}>
       <ThemeProvider theme={theme}>
         <Container>
-          <AppBar>
-            <Toolbar>
-              <div>
-                <SearchIcon />
-                <StyledInput placeholder='Search...' />
-              </div>
-            </Toolbar>
-          </AppBar>
-          <TextField placeholder='Search' onChange={onChange} />
-          <Button variant='contained' onClick={onClickSearch}>
-            Search
-          </Button>
-          <pre>
-            <code>{result}</code>
-          </pre>
+          <AppBarContainer>
+            <AppBar position='static'>
+              <EnhancedToolbar>
+                <Title>Podcasts</Title>
+                <SearchContainer>
+                  <SearchIconContainer>
+                    <SearchIcon />
+                  </SearchIconContainer>
+                  <StyledInput placeholder='Search...' onChange={onSearchChange} />
+                </SearchContainer>
+              </EnhancedToolbar>
+            </AppBar>
+          </AppBarContainer>
         </Container>
       </ThemeProvider>
     </MuiThemeProvider>
   )
 }
 
+const AppBarContainer = styled.div`
+  flex-grow: 1;
+`
+
+const EnhancedToolbar = styled(Toolbar)`
+  justify-content: space-between;
+`
+
+const SearchContainer = styled.div`
+  ${({ theme }) => `
+    position: relative;
+    border-radius: ${theme.shape.borderRadius}px; 
+    background-color: ${fade(theme.palette.common.white, 0.15)};
+    &:hover {
+      background-color: ${fade(theme.palette.common.white, 0.25)};
+    }
+    margin-right: ${theme.spacing(2)}px;
+    margin-left: 0;
+    width: 100%;
+    ${theme.breakpoints.up('sm')} {
+      margin-left: ${theme.spacing(3)}px;
+      width: auto;
+    }
+  `}
+`
+
 const StyledInput = styled(InputBase)`
-  input {
-    background-color: ${({ theme }) => theme.palette.primary.main};
-  }
+  ${({ theme }) => `
+    input {
+      color: inherit;
+      padding: ${theme.spacing(1, 1, 1, 0)};
+      padding-left: calc(1em + ${theme.spacing(4)}px);
+      transition: ${theme.transitions.create('width')};
+      width: 100%;
+      ${theme.breakpoints.up('md')} {
+        width: 12ch;
+        &:focus {
+          width: 20ch;
+        }
+      }
+    }
+  `}
+`
+
+const SearchIconContainer = styled(SearchIcon)`
+  ${({ theme }) => `
+    padding: ${theme.spacing(0, 2)};
+    height: 100%;
+    position: absolute;
+    pointer-events: none;
+    display: flex;
+    alignItems: center;
+    justifyContent: center;
+  `}
+`
+
+const Title = styled(Typography)`
+  ${({ theme }) => `
+    display: none;
+    ${theme.breakpoints.up('sm')} {
+      display: block;
+    },
+  `}
 `
