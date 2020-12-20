1fc81c8dcd58410182857ac5f45ca47d3a366b83
Jaan Lambert <jaan.lambert@gmail.com>
2020-12-20 18:57:35 +0200

    Style search results

--- src/client/SearchResults.tsx
+++ src/client/SearchResults.tsx
@@ -1,17 +1,52 @@
+import styled from '@emotion/styled'
 import * as React from 'react'
 import { SearchResult } from './api'
+import { Grid, Card, CardMedia, CardContent, Typography } from '@material-ui/core'
 
 interface Props {
   results: SearchResult[]
 }
 const SearchResults = ({ results }: Props): React.ReactElement => {
   return (
-    <div>
-      {results.map((result, i) => (
-        <div key={i}>{result.artistName}</div>
-      ))}
-    </div>
+    <Root>
+      <Grid container spacing={3}>
+        {results.map((result, i) => (
+          <Grid key={i} item xs={6}>
+            <EnhancedCard>
+              <EnhancedCardMedia image={result.artworkUrl100} />
+              <ContentContainer>
+                <EnhancedCardContent>
+                  <Typography component='h2'>{result.artistName}</Typography>
+                </EnhancedCardContent>
+              </ContentContainer>
+            </EnhancedCard>
+          </Grid>
+        ))}
+      </Grid>
+    </Root>
   )
 }
+
+const Root = styled.div`
+  padding-top: ${({ theme }) => theme.spacing(3)}px;
+`
+
+const EnhancedCard = styled(Card)`
+  display: flex;
+  background-color: ${({ theme }) => theme.palette.background.default};
+`
+
+const EnhancedCardMedia = styled(CardMedia)`
+  width: 100px;
+  height: 100px;
+`
+
+const ContentContainer = styled.div`
+  display: flex;
+`
+
+const EnhancedCardContent = styled(CardContent)`
+  flex: 1 0 auto;
+`
 
 export default SearchResults
