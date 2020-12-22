dbc2bff5c32e024d4e69445af1d4b25b9ae3f20a
Jaan Lambert <jaan.lambert@gmail.com>
2020-12-22 23:38:28 +0200

    Add global styles

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -2,7 +2,7 @@ import { debounce } from 'lodash'
 import * as React from 'react'
 import { Container } from '@material-ui/core'
 import { createMuiTheme, ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles'
-import { ThemeProvider } from '@emotion/react'
+import { ThemeProvider, Global, css } from '@emotion/react'
 import TopBar from './TopBar'
 import SearchResults from './Search/SearchResults'
 import { fetchSearchResults } from './api'
@@ -24,6 +24,14 @@ export const App: React.FC = () => {
   return (
     <MuiThemeProvider theme={theme}>
       <ThemeProvider theme={theme}>
+        <Global
+          styles={css`
+            a {
+              color: inherit;
+              text-decoration: inherit;
+            }
+          `}
+        />
         <Container>
           <TopBar onSearchChange={onSearchChange} />
           <Router>
